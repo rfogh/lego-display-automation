e392d3b78b7afda92e2ee6340967575a13f82bc7
Rune Fogh <rfogh@rfogh.dk>
2018-08-14 15:14:00 +0200

    Added Process manager and Face detection for RPi

---
+++ facedetection/mqtt_face_detection.py
@@ -0,0 +1,59 @@
+#MQTT
+import paho.mqtt.client as mqtt
+#OpenCV
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import sys
+import imutils
+import os
+
+# Create the haar cascade
+cascPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "haarcascade_frontalface_default.xml")
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (320, 240)
+camera.framerate = 4
+rawCapture = PiRGBArray(camera, size=(320, 240))
+
+client = mqtt.Client()
+client.connect("192.168.1.2")
+
+client.loop_start()
+
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+    image = frame.array
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    if len(faces) > 0:
+        client.publish("event/facedetected", len(faces))
+        time.sleep(4)
+    
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+ 
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    
+    time.sleep(1)
+        
+client.loop_stop()
---
+++ processmanager/processmanager.py
@@ -0,0 +1,74 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+import random
+
+class MqttClient:
+    
+    host = "192.168.1.2"
+
+    def __init__(self, handler, subscriptions):
+        self.client = mqtt.Client()
+        self.client.connect(self.host)
+        self.client.on_message = self.__on_message
+        self.client.on_connect = self.__on_connect
+        self.handler = handler
+        self.on_connect = subscriptions
+                
+    def __on_message(self, client, userdata, message):
+        topic = message.topic
+        payload = {}
+        if (message.payload):
+            payload = json.loads(str(message.payload.decode("utf-8")))
+        self.handler(topic, payload)
+        
+    def __on_connect(self, client, userdata, flags, rc):
+        self.on_connect()
+                
+    def publish(self, topic, message={}):
+        jsonMessage = json.dumps(message)
+        print("Publish to "+ topic +": "+ jsonMessage)
+        self.client.publish(topic, jsonMessage)
+        
+    def subscribe(self, topic):
+        self.client.subscribe(topic)
+        
+    def loop_forever(self):
+        self.client.loop_forever()
+
+def getRandomColor():
+    color = 0
+    while color == 0:
+        color = random.randint(0,1)*0xFF0000 + random.randint(0,1)*0xFF00 + random.randint(0,1)*0xFF
+    return color
+
+def handler(topic, message):
+    if (topic == "event/ferris/activated"):
+        client.publish("command/ferris/run", {"direction":0, "duration":10, "pause":5})
+    elif (topic == "event/facedetected"):
+        circle = random.randint(0,1) == 1
+        mode = random.randint(0,55)
+        colors = [getRandomColor(), getRandomColor(), getRandomColor()]
+        speed = 1000 if circle else 7000
+        direction = random.randint(0,2)
+        client.publish("command/ferris/setmode", {"circle":circle,"mode":mode,"colors":colors,"speed":speed,"direction":direction})
+
+def subscriptions():
+    client.subscribe("event/facedetected")
+    client.subscribe("event/ferris/activated")
+
+client = MqttClient(handler, subscriptions)
+
+
+##client.publish("command/ferris/setmode", {"circle":True,"mode":44,"colors":[255],"speed":1000,"direction":0})
+##client.publish("command/ferris/start", {"direction":0})
+##time.sleep(5)
+##client.publish("command/ferris/stop")
+##time.sleep(2)
+##client.publish("command/ferris/setmode", {"circle":True,"mode":44,"colors":[65280],"speed":1000,"direction":1})
+##client.publish("command/ferris/start", {"direction":1})
+##time.sleep(5)
+##client.publish("command/ferris/stop")
+##client.publish("command/ferris/setmode", {"circle":False,"mode":44,"colors":[255],"speed":10000,"direction":0})
+
+client.loop_forever()
